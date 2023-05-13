249e57422854ce289958dbae3bd30097da57bb53
Sam Chapman <sam.chapman@outlook.com>
2023-05-13 22:51:00 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Personal Development Blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
