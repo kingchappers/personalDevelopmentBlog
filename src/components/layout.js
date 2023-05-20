31b3558514f8133eeb0409e904de70e7574d6536
Sam Chapman <sam.chapman@outlook.com>
2023-05-20 09:01:44 +0100

    added header element for header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,40 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 
 const Layout = ({ pageTitle, children }) => {
+
+    const data = useStaticQuery(graphql`
+        query MyQuery {
+            site {
+            siteMetadata {
+                title
+            }
+            }
+        }
+    `)
+
     return (
         <div className="max-w-screen-7xl mx-20">
-            <h1 className="text-4xl font-bold mt-5">
-                Sam Chapman .Dev | Personal Development Blog
-            </h1>
-            <nav className="flex items-center justify-between flex-wrap bg-slate-800 rounded px-5 py-5 mt-4 mb-4">
-                <ul className="flex">
-                    <li className="mr-6">
-                        <Link className="rounded py-2 px-4 bg-slate-600 hover:bg-slate-700 text-white" to="/">
-                            Home
-                        </Link>
-                    </li>
-                    <li className="mr-6">
-                        <Link className="rounded py-2 px-4 bg-slate-600 hover:bg-slate-700 text-white" to="/about">
-                            About
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
+            <header>
+                <h1 className="text-4xl font-bold mt-5">
+                    Sam Chapman .Dev | {data.site.siteMetadata.title}
+                </h1>
+                <nav className="flex items-center justify-between flex-wrap bg-slate-800 rounded px-5 py-5 mt-4 mb-4">
+                    <ul className="flex">
+                        <li className="mr-6">
+                            <Link className="rounded py-2 px-4 bg-slate-600 hover:bg-slate-700 text-white" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="mr-6">
+                            <Link className="rounded py-2 px-4 bg-slate-600 hover:bg-slate-700 text-white" to="/about">
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            
             <main>
                 <h1 className="text-2xl font-bold pb-3">{pageTitle}</h1>
                 {children}
