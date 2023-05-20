e8a63d6cd9eb6417e915b5155a6eb3fad692d383
Sam Chapman <sam.chapman@outlook.com>
2023-05-20 08:49:50 +0100

    Updated navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,26 +3,26 @@ import { Link } from 'gatsby'
 
 const Layout = ({ pageTitle, children }) => {
     return (
-        <div className="m-10 max-w-screen-2xl mx-auto px-20">
-            <h1 className="text-3xl font-bold">
+        <div className="max-w-screen-7xl mx-20">
+            <h1 className="text-4xl font-bold mt-5">
                 Sam Chapman .Dev | Personal Development Blog
             </h1>
-            <nav className="flex items-center justify-between flex-wrap bg-slate-800 p-4 mt-4 mb-4">
+            <nav className="flex items-center justify-between flex-wrap bg-slate-800 rounded px-5 py-5 mt-4 mb-4">
                 <ul className="flex">
                     <li className="mr-6">
-                        <Link className="inline-block border border-slate-500 rounded py-2 px-4 bg-slate-500 hover:bg-slate-700 text-white" to="/">
+                        <Link className="rounded py-2 px-4 bg-slate-600 hover:bg-slate-700 text-white" to="/">
                             Home
                         </Link>
                     </li>
                     <li className="mr-6">
-                        <Link className="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white" to="/about">
+                        <Link className="rounded py-2 px-4 bg-slate-600 hover:bg-slate-700 text-white" to="/about">
                             About
                         </Link>
                     </li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitle}</h1>
+                <h1 className="text-2xl font-bold pb-3">{pageTitle}</h1>
                 {children}
             </main>
         </div>
