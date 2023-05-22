a68126d8669debe1e4870f7670f7608c31d998e6
Sam Chapman <sam.chapman@outlook.com>
2023-05-22 12:56:53 +0100

    uninstalled prismjs-syntax highlights not working

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import './src/styles/global.css'
 
 //require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-//require("prismjs/themes/prism-solarizedlight.css")
+// require("prismjs/themes/prism-solarizedlight.css")
 // require("prismjs/plugins/command-line/prism-command-line.css")
