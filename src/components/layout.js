f94b4461b59c2d8d03b552cd51e3f4af93d311c0
Sam Chapman <sam.chapman@outlook.com>
2023-05-29 08:06:06 +0100

    Changed max width

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ const Layout = ({ pageTitle, children }) => {
     return (
         <div className="grid place-content-center mx-10">
             <header>
-                <h1 className="text-5xl font-bold mt-5">
+                <h1 className="max-w-screen-xl text-5xl font-bold mt-5">
                     Sam Chapman .Dev | {data.site.siteMetadata.title}
                 </h1>
                 <nav className="flex items-center justify-between flex-wrap bg-slate-800 rounded px-5 py-5 mt-4 mb-4">
@@ -42,7 +42,7 @@ const Layout = ({ pageTitle, children }) => {
                 </nav>
             </header>
 
-            <main className="max-w-screen-xl col-span-full">
+            <main className="col-span-full">
                 <h1 className="text-4xl font-bold pb-3">{pageTitle}</h1>
                 {children}
             </main>
