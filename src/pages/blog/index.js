34833e31031a20c91c2c0537a96a41464899f34a
Sam Chapman <sam.chapman@outlook.com>
2023-05-30 17:48:20 +0100

    Cards appearing in rows

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,44 +7,25 @@ import "../../styles/global.scss"
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Blog Posts">
+      <div className="columns is-multiline">  
         {
           data.allMdx.nodes.map(node => (
-            <div className="card mb-4">
-              <div className="card-content">
-            <article key={node.id}>
-              <h2 className="card-header-title">
-                <Link to={`/blog/${node.frontmatter.slug}`}>
-                  {node.frontmatter.title}
-                </Link>
-              </h2>
-              <p className="content">Excerpt: {node.excerpt}</p>
-              <p className="is-size-7">Posted: {node.frontmatter.date}</p>
-            </article>
+            <div className="column is-one-quarter">
+                <div className="card card-content">
+                  <article key={node.id}>
+                    <h2 className="card-header-title">
+                      <Link to={`/blog/${node.frontmatter.slug}`}>
+                        {node.frontmatter.title}
+                      </Link>
+                    </h2>
+                    <p className="content">Excerpt: {node.excerpt}</p>
+                    <p className="is-size-7">Posted: {node.frontmatter.date}</p>
+                  </article>
             </div>
-            </div>
+        </div>
           ))
         }
-
-
-      <ul>
-        {
-          data.allMdx.nodes.map(node => (
-            <div className="grid-cols-2 max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
-              <div>
-            <article key={node.id}>
-              <h2 className='text-xl font-bold'>
-                <Link to={`/blog/${node.frontmatter.slug}`}>
-                  {node.frontmatter.title}
-                </Link>
-              </h2>
-              <p>Excerpt: {node.excerpt}</p>
-              <p className='text-sm pb-3'>Posted: {node.frontmatter.date}</p>
-            </article>
-            </div>
-            </div>
-          ))
-        }
-      </ul>
+        </div>
     </Layout>
   )
 }
