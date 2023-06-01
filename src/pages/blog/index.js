5cf69544f4faa07692bebfd24f90db4ad4fdaaae
Sam Chapman <sam.chapman@outlook.com>
2023-06-01 14:49:45 +0100

    Further style updates and sorted cards on blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,9 +7,115 @@ import "../../styles/global.scss"
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Blog Posts">
+      <h2 className="subtitle has-text-white-ter">General Tools</h2>
       <div className="columns is-multiline">  
         {
-          data.allMdx.nodes.map(node => (
+          data.generalTools.nodes.map(node => (
+            <div className="column is-one-quarter">
+                <div className="card card-content has-text-white-ter">
+                <Link to={`/blog/${node.frontmatter.slug}`}>
+                  <article key={node.id}>
+                    <h2 className="card-header-title has-text-white-ter">
+                      {node.frontmatter.title}
+                    </h2>
+                    <p className="content">Excerpt: {node.excerpt}</p>
+                    <p className="is-size-7">Posted: {node.frontmatter.date}</p>
+                  </article>
+                  </Link>
+            </div>
+        </div>
+          ))
+        }
+        </div>
+
+        <h2 className="subtitle has-text-white-ter">Infrastructure Management</h2>
+        <div className="columns is-multiline">  
+        {
+          data.infrastructureManagement.nodes.map(node => (
+            <div className="column is-one-quarter">
+                <div className="card card-content has-text-white-ter">
+                <Link to={`/blog/${node.frontmatter.slug}`}>
+                  <article key={node.id}>
+                    <h2 className="card-header-title has-text-white-ter">
+                      {node.frontmatter.title}
+                    </h2>
+                    <p className="content">Excerpt: {node.excerpt}</p>
+                    <p className="is-size-7">Posted: {node.frontmatter.date}</p>
+                  </article>
+                  </Link>
+            </div>
+        </div>
+          ))
+        }
+        </div>
+
+        <h2 className="subtitle has-text-white-ter">Linux Configuration</h2>
+        <div className="columns is-multiline">  
+        {
+          data.linuxConfiguration.nodes.map(node => (
+            <div className="column is-one-quarter">
+                <div className="card card-content has-text-white-ter">
+                <Link to={`/blog/${node.frontmatter.slug}`}>
+                  <article key={node.id}>
+                    <h2 className="card-header-title has-text-white-ter">
+                      {node.frontmatter.title}
+                    </h2>
+                    <p className="content">Excerpt: {node.excerpt}</p>
+                    <p className="is-size-7">Posted: {node.frontmatter.date}</p>
+                  </article>
+                  </Link>
+            </div>
+        </div>
+          ))
+        }
+        </div>
+
+        <h2 className="subtitle has-text-white-ter">Linux Tools</h2>
+        <div className="columns is-multiline">  
+        {
+          data.linuxTools.nodes.map(node => (
+            <div className="column is-one-quarter">
+                <div className="card card-content has-text-white-ter">
+                <Link to={`/blog/${node.frontmatter.slug}`}>
+                  <article key={node.id}>
+                    <h2 className="card-header-title has-text-white-ter">
+                      {node.frontmatter.title}
+                    </h2>
+                    <p className="content">Excerpt: {node.excerpt}</p>
+                    <p className="is-size-7">Posted: {node.frontmatter.date}</p>
+                  </article>
+                  </Link>
+            </div>
+        </div>
+          ))
+        }
+        </div>
+
+        <h2 className="subtitle has-text-white-ter">Security Tools</h2>
+        <div className="columns is-multiline">  
+        {
+          data.securityTools.nodes.map(node => (
+            <div className="column is-one-quarter">
+                <div className="card card-content has-text-white-ter">
+                <Link to={`/blog/${node.frontmatter.slug}`}>
+                  <article key={node.id}>
+                    <h2 className="card-header-title has-text-white-ter">
+                      {node.frontmatter.title}
+                    </h2>
+                    <p className="content">Excerpt: {node.excerpt}</p>
+                    <p className="is-size-7">Posted: {node.frontmatter.date}</p>
+                  </article>
+                  </Link>
+            </div>
+        </div>
+          ))
+        }
+        </div>
+
+        <h2 className="subtitle has-text-white-ter">Windows Tools</h2>
+        <div className="columns is-multiline">  
+        {
+          data.windowsTools.nodes.map(node => (
             <div className="column is-one-quarter">
                 <div className="card card-content has-text-white-ter">
                 <Link to={`/blog/${node.frontmatter.slug}`}>
@@ -31,19 +137,95 @@ const BlogPage = ({ data }) => {
 }
 
 export const query = graphql `
-  query GetBlogPosts {
-    allMdx(sort: {frontmatter: {date: DESC}}) {
-      nodes {
-        frontmatter {
-          date(formatString: "D MMMM, YYYY")
-          title
-          slug
-        }
-        excerpt(pruneLength: 100)
+query GetBlogPosts {
+  generalTools: allMdx(
+    sort: {frontmatter: {date: DESC}}
+    filter: {frontmatter: {category: {eq: "General Tools"}}}
+  ) {
+    nodes {
+      frontmatter {
+        date(formatString: "D MMMM, YYYY")
+        title
+        slug
+        category
       }
+      excerpt(pruneLength: 100)
     }
   }
+  infrastructureManagement: allMdx(
+    sort: {frontmatter: {date: DESC}}
+    filter: {frontmatter: {category: {eq: "Infrastructure Management"}}}
+  ) {
+    nodes {
+      frontmatter {
+        date(formatString: "D MMMM, YYYY")
+        title
+        slug
+        category
+      }
+      excerpt(pruneLength: 100)
+    }
+  }
+  linuxConfiguration: allMdx(
+    sort: {frontmatter: {date: DESC}}
+    filter: {frontmatter: {category: {eq: "Linux Configuration"}}}
+  ) {
+    nodes {
+      frontmatter {
+        date(formatString: "D MMMM, YYYY")
+        title
+        slug
+        category
+      }
+      excerpt(pruneLength: 100)
+    }
+  }
+	linuxTools: allMdx(
+    sort: {frontmatter: {date: DESC}}
+    filter: {frontmatter: {category: {eq: "Linux Tools"}}}
+  ) {
+    nodes {
+      frontmatter {
+        date(formatString: "D MMMM, YYYY")
+        title
+        slug
+        category
+      }
+      excerpt(pruneLength: 100)
+    }
+  }
+  securityTools: allMdx(
+    sort: {frontmatter: {date: DESC}}
+    filter: {frontmatter: {category: {eq: "Security Tools"}}}
+  ) {
+    nodes {
+      frontmatter {
+        date(formatString: "D MMMM, YYYY")
+        title
+        slug
+        category
+      }
+      excerpt(pruneLength: 100)
+    }
+  }
+  windowsTools: allMdx(
+    sort: {frontmatter: {date: DESC}}
+    filter: {frontmatter: {category: {eq: "Windows Tools"}}}
+  ) {
+    nodes {
+      frontmatter {
+        date(formatString: "D MMMM, YYYY")
+        title
+        slug
+        category
+      }
+      excerpt(pruneLength: 100)
+    }
+  }
+}
 `
+
+
 
 export const Head = () => <Seo title="Blog Posts" />
 
