e6ef47f4939920cff6c051a332da6ecb8ebf1792
Sam Chapman <sam.chapman@outlook.com>
2023-06-02 09:53:31 +0100

    Minor fixes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,3 @@
-
-
 //require("prismjs/plugins/line-numbers/prism-line-numbers.css")
 //require("prismjs/themes/prism-okaidia.css")
 // require("prismjs/plugins/command-line/prism-command-line.css")
